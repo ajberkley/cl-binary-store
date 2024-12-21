50dc80c3d4b25a3d5ef6b6d3c9e5a2fc6c6b264a
ajberkley <ajberkley@gmail.com>
2024-12-21 06:56:51 -0800

    missing file

---
+++ src/features.lisp
@@ -0,0 +1,4 @@
+;; To enable debugging execute the below line and recompile everything
+;; (pushnew :debug-csf *features*)
+;; To disable debugging execute the below and recompile everything
+;; (setf *features* (delete :debug-csf *Features*))
