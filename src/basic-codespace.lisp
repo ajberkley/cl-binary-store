6a7652aa4a2c79054c9e87d0079d685696e41e80
ajberkley <ajberkley@gmail.com>
2025-02-09 19:09:59 -0800

    Remove accidental debug commit

--- src/basic-codespace.lisp
+++ src/basic-codespace.lisp
@@ -17,7 +17,7 @@
   "This is the basic codespace of cl-binary-store.")
 
 ;; Enable debug to get source code saved to a file so the debugger does the right thing
-(define-codespace ("basic codespace" +basic-codespace+ :debug t)
+(define-codespace ("basic codespace" +basic-codespace+ :debug nil)
   (register-references num-eq-refs (make-hash-table :test #'eq :size *num-eq-refs-table-size*))
   (register-references
    double-float-refs (make-hash-table :test #+sbcl #'double-float-= #-sbcl #'eql
